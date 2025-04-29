ec33fbc6a3bbc1c7194d5e298cfbcfebd5adc933
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-04-29 18:52:16 +0200

    Added some dependencies

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-# Package installation script
+from setuptools import setup, find_packages
+
+setup(
+    name="postgresql-practice",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        "SQLAlchemy>=1.4.0",
+        "psycopg2-binary>=2.9.0",
+    ],
+    python_requires=">=3.6",
+)
