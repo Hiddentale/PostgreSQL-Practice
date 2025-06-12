4d8c8c123bf9fd84257df14dd4195ee29c1b531f
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-06-12 09:56:49 +0200

    Added join functionality, but struggling with multiple joins chaining

--- src/database/query_executors.py
+++ src/database/query_executors.py
@@ -9,6 +9,11 @@ class QueryBuilder:
         self.table = None
         self.columns: Optional[List] = None
         self.where_string = None
+        self.in_join = None
+        self.l_join = None
+        self.r_join = None
+        self.fo_join = None
+        self.crss_join = None
 
     def __str__(self):
         if not self.table:
@@ -29,12 +34,28 @@ class QueryBuilder:
         if self.where_string:
             where_str_str = ",".join(self.where_string)
             sql_string.append(f"WHERE {where_str_str}")
+
+        if self.in_join:
+            in_join_str = f"INNER JOIN {self.in_join[0]} ON {self.in_join[1]}"
+            sql_string.append(in_join_str)
+
+        if self.l_join:
+            l_join_str = f"LEFT JOIN {self.l_join[0]} ON {self.l_join[1]}"
+            sql_string.append(l_join_str)
         
-        print(sql_string)
+        if self.r_join:
+            r_join_str = f"RIGHT JOIN {self.r_join[0]} ON {self.r_join[1]}"
+            sql_string.append(r_join_str)
+
+        if self.fo_join:
+            fo_join_str = f"FULL OUTER JOIN {self.fo_join[0]} ON {self.fo_join[1]}"
+            sql_string.append(fo_join_str)
+
+        if self.crss_join:
+            crss_join_str = f"CROSS JOIN {self.crss_join}"
+            sql_string.append(crss_join_str)
 
         return "\n".join(sql_string)
-    
-        # Proper SQL Query format here
 
     # # ______________________________Core Query Operations________________________________
     def count(self):
@@ -63,22 +84,27 @@ class QueryBuilder:
         return self
 
     # ______________________________Joins________________________________
-    def cross_join(self):
+    def cross_join(self, crss_join: str):
+        self.crss_join = crss_join
         return self
 
-    def full_outer_join(self):
+    def full_outer_join(self, fo_join_1: str, fo_join_2: str):
+        self.fo_join = (fo_join_1, fo_join_2)
         return self
 
-    def inner_join(self):
+    def inner_join(self, in_join_1: str, in_join_2: str):
+        self.in_join = (in_join_1, in_join_2)
         return self
 
     def join(self):
         return self
 
-    def left_join(self):
+    def left_join(self, l_join_1: str, l_join_2: str):
+        self.l_join = (l_join_1, l_join_2)
         return self
 
-    def right_join(self):
+    def right_join(self, r_join_1: str, r_join_2: str):
+        self.r_join = (r_join_1, r_join_2)
         return self
 # ______________________________Query Structure________________________________
     def from_table(self, table: str): # "Explicitely disallow comma notation and table functions for security.
